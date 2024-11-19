668c7e50e0aadad6d7c08cc65e6e2f822e21aa50
Rizky Hanifa Afania <rizkyhanifa33@gmail.com>
2024-11-19 11:06:33 +0700

    Menambahkan Guided

---
+++ 10_Data_Storage_Bagian_1/GUIDED/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:pert_10/view/my_db_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Database Storage',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyDatabaseView(),
+    );
+  }
+}
