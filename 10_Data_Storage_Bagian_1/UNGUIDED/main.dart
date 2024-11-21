12d0bc4c0310fc3196e3fc042cfc1eb1f5eb0438
Rizky Hanifa Afania <rizkyhanifa33@gmail.com>
2024-11-21 13:49:50 +0700

    Menambahkan unguided

---
+++ 10_Data_Storage_Bagian_1/UNGUIDED/main.dart
@@ -0,0 +1,23 @@
+import 'package:belajar_sqlite/view/main_view.dart';
+import 'package:belajar_sqlite/view/my_db_view.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Unguided Pertemuan 10',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MainView(),
+    );
+  }
+}
